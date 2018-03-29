90500dfa2def8b6c021e5678cb75f09853ede8f6
Heidi Lyons <heidishh46@gmail.com>
2018-03-28 20:32:47 -0700

    cleaned up code
    Signed-off-by: Heidi Lyons <heidishh46@gmail.com>

--- analyze.py
+++ analyze.py
@@ -27,6 +27,7 @@ def clean_tweet(tweet):
         '''
         return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
+''' https://www.geeksforgeeks.org/twitter-sentiment-analysis-using-python/ '''
 def get_tweet_sentiment(tweet):
         '''
         Utility function to classify sentiment of passed tweet
@@ -43,6 +44,7 @@ def get_tweet_sentiment(tweet):
             return 'negative'
 
 def get_text_tweets():
+    ''' Extract the text out of the Tweet metadata, add to global var userTweets '''
     global userInfo
     global userTweets
     for user in userInfo:
@@ -50,7 +52,29 @@ def get_text_tweets():
         #print(user["text"])
     
 
+def printSentiments():
+    ''' print out a sampling of user sentiments in each category '''
+    global neutralsentiment, psentiment, nsentiment
+    print("10 neutral tweets: ")
+    for i in range(10):
+        print(neutralsentiment[i])
+        print('\n')
+
+    print("10 positive tweets: ")
+    for i in range(10):
+        print(psentiment[i])
+        print('\n')
+
+    print("10 negative tweets: ")
+    for i in range(10):
+        print(nsentiment[i])
+        print('\n')
+
+
 def analyzeSentiment():
+    ''' Categorize textual sentiment using textblob, categorize into lists of 
+        positive (psentiment), negative (nsentiment) and neutral (neutralsentiment) 
+    '''
     global userTweets
     global sentiments
     global psentiment, nsentiment, neutralsentiment
@@ -67,6 +91,7 @@ def analyzeSentiment():
 if __name__ == "__main__":
     global userInfo, userLocations, userTweets, sentiments
     global psentiment, nsentiment, neutralsentiment
+    
     userInfo=[]
     userLocations=[]
     userTweets=[]
@@ -78,19 +103,12 @@ if __name__ == "__main__":
 
     findUserLocation()
     
-    #print(userInfo)
-    #print(userLocations)
 
     # obtain sentiment analysis
     get_text_tweets()
     analyzeSentiment()
 
-    print("neutral tweets: ")
-    for i in range(10):
-        print(neutralsentiment[i])
-        print('\n\n')
-
-    #print(Counter(sentiments))
+    printSentiments()
     
 
     #print(Counter(userLocations))
